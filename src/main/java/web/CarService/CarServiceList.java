e8e86b76662409194df1114c288cb9f449e0d627
Yosyf Akhmetov <Avtograd-Rabota2020@yandex.ru>
2023-02-23 19:08:14 +0300

    Completed

--- src/main/java/web/CarService/CarServiceList.java
+++ src/main/java/web/CarService/CarServiceList.java
@@ -6,6 +6,8 @@ import web.models.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceList {
@@ -19,15 +21,17 @@ public class CarServiceList {
         carList.add(new Car(3, "Mercedes E200", 1999));
         carList.add(new Car(4, "Lexus IS250", 2006));
         carList.add(new Car(5, "Datsun ON-DO", 2018));
-
-
     }
-
     public List<Car> index() {
         return carList;
     }
     public Car show(int id){
         return carList.stream().filter(e->e.getId()==id).findAny().orElse(null);
     }
-
+    public List<Car> count(int count){
+        if (count == 0) {
+            return carList;
+        }
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
 }
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,27 +6,40 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.CarService.CarServiceList;
+import web.models.Car;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
 
     private CarServiceList carServiceList;
+
     @Autowired
-    public CarController(CarServiceList carServiceList){
+    public CarController(CarServiceList carServiceList) {
         this.carServiceList = carServiceList;
     }
 
     @GetMapping("")
-    public String cars(Model model){  // получим все автомобили
-        model.addAttribute("cars",carServiceList.index());
+    public String cars(Model model) {  // получим все автомобили
+        model.addAttribute("cars", carServiceList.index());
         return "cars";
     }
+
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id,Model model){  // получим машину по id
-        model.addAttribute("car",carServiceList.show(id));
+    public String show(@PathVariable("id") int id, Model model) {  // получим машину по id
+        model.addAttribute("car", carServiceList.show(id));
         return "show";
+    }
+    @GetMapping("/count")
+    public String count(Model model, @RequestParam(value = "count",required = false) Integer count){
+        List<Car> cars = carServiceList.count(count);
+        model.addAttribute("cars",cars);
+
+        return "count";
     }
 
 }

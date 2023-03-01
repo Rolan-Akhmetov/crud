4ad7dabb59eea8a26d46bab3369623d144190722
Yosyf Akhmetov <Avtograd-Rabota2020@yandex.ru>
2023-03-01 22:11:01 +0300

    Completed

--- src/main/java/web/dao/CarDao.java
+++ src/main/java/web/dao/CarDao.java
@@ -1,12 +1,10 @@
 package web.dao;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import web.models.Car;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
@@ -25,7 +23,6 @@ public class CarDao{
 
     }
 
-    @Transactional(readOnly = true)
     public List<Car> getAll() {
         Session session = sessionFactory.getCurrentSession();
         List<Car> cars = session.createQuery("select c from Car c", Car.class).getResultList();
@@ -33,25 +30,25 @@ public class CarDao{
         return cars;
     }
 
-    @Transactional(readOnly = true)
+
     public Car getById(int id) {
         Session session = sessionFactory.getCurrentSession();
         return session.get(Car.class, id);
     }
 
-    @Transactional
+
     public void save(Car car) {
         Session session = sessionFactory.getCurrentSession();
         session.save(car);
     }
 
-    @Transactional
+
     public void update(Car updateCar) {
         Session session = sessionFactory.getCurrentSession();
         session.saveOrUpdate(updateCar);
     }
 
-    @Transactional
+
     public void delete(int id) {
         Session session = sessionFactory.getCurrentSession();
         session.remove(session.get(Car.class,id));
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -2,41 +2,38 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.CarDao;
 import web.models.Car;
 
 import java.util.List;
 
 @Service
-public class CarService implements web.service.Service<Car> {
+@Transactional
+public class CarService{
 
-    private CarDao carDao;
+    private final CarDao carDao;
     @Autowired
     public CarService(CarDao carDao) {
         this.carDao = carDao;
     }
 
-    @Override
+
     public List<Car> getAll() {
         return carDao.getAll();
     }
 
-    @Override
+
     public Car getById(int id) {
         return carDao.getById(id);
     }
 
-    @Override
     public void save(Car car) {
         carDao.save(car);
     }
-
-    @Override
     public void update(Car updateCar) {
         carDao.update(updateCar);
     }
-
-    @Override
     public void delete(int id) {
         carDao.delete(id);
     }
